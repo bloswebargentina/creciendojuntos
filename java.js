758924ee1ba9e41f1739d851b029a4566f3e15e7
bloswebargentina <trabajoisa23messi@gmail.com>
2024-08-23 16:32:18 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+document.getElementById('boton').onclick = function() {
+    window.location.href = '../pagina anuncios/paginas/nosotros.html'; 
+};
+
+document.getElementById('hijocontenido1').onclick = function() {
+    window.location.href = '../pagina anuncios/paginas/tips/dormir.html'; 
+};
+
+document.getElementById('hijocontenido2').onclick = function() {
+    window.location.href = '../pagina anuncios/paginas/tips/comer.html'; 
+};
+
+document.getElementById('hijocontenido3').onclick = function() {
+    window.location.href = '../pagina anuncios/paginas/tips/juegos.html'; 
+};
