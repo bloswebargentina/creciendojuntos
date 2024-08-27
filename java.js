430e33a5ba720c03338f33a593f8889793f4bbdc
bloswebargentina <trabajoisa23messi@gmail.com>
2024-08-27 16:34:30 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,15 +1,52 @@
 document.getElementById('boton').onclick = function() {
-    window.location.href = '../pagina anuncios/paginas/nosotros.html'; 
+    window.location.href = '../creciendojuntos/pages/nosotros.html'; 
 };
 
 document.getElementById('hijocontenido1').onclick = function() {
-    window.location.href = '../pagina anuncios/paginas/tips/dormir.html'; 
+    window.location.href = '../creciendojuntos/pages/tips/dormir.html'; 
 };
 
 document.getElementById('hijocontenido2').onclick = function() {
-    window.location.href = '../pagina anuncios/paginas/tips/comer.html'; 
+    window.location.href = '../creciendojuntos/pages/tips/comer.html'; 
 };
 
 document.getElementById('hijocontenido3').onclick = function() {
-    window.location.href = '../pagina anuncios/paginas/tips/juegos.html'; 
+    window.location.href = '../creciendojuntos/pages/tips/juegos.html'; 
 };
+
+document.getElementById('hijocontenido4').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/higeneinfantil.html'; 
+};
+
+document.getElementById('hijocontenido5').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/actividadfisica.html'; 
+};
+
+document.getElementById('hijocontenido6').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/libertad.html'; 
+};
+
+document.getElementById('hijocontenido7').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/desarolloemocionalgeneral.html'; 
+};
+
+document.getElementById('hijocontenido8').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/eschuchaactiva.html'; 
+};
+
+document.getElementById('hijocontenido9').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/malcomportamiento.html'; 
+};
+
+document.getElementById('hijocontenido10').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/tiempodecalidad.html'; 
+};
+
+document.getElementById('hijocontenido11').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/entornofamiliar.html'; 
+};
+
+document.getElementById('hijocontenido12').onclick = function() {
+    window.location.href = '../creciendojuntos/pages/tips/descubriremociones.html'; 
+};
+
