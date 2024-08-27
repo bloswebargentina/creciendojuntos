71c08f175007969698abc1f21b3e79b299854f09
bloswebargentina <trabajoisa23messi@gmail.com>
2024-08-27 16:42:42 -0300

    Add files via upload

---
+++ pages/javanosotros.js
@@ -0,0 +1,15 @@
+document.getElementById('h1barra').onclick = function() {
+    window.location.href = '../index.html'; 
+};
+
+document.getElementById('hijocontenido1').onclick = function() {
+    window.location.href = '../pages/tips/dormir.html'; 
+};
+
+document.getElementById('hijocontenido2').onclick = function() {
+    window.location.href = '../pages/tips/malcomportamiento.html'; 
+};
+
+document.getElementById('hijocontenido3').onclick = function() {
+    window.location.href = '../pages/tips/desarolloemocionalgeneral.html'; 
+};
---
+++ pages/tips/javatips.js
@@ -0,0 +1,7 @@
+document.getElementById('h1barra').onclick = function() {
+    window.location.href = '../../index.html'; 
+};
+
+document.getElementById('boton').onclick = function() {
+    window.location.href = '../../pages/nosotros.html'; 
+};
